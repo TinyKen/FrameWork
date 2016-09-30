761576add02305b80be92e88400741405be68acd
xxg3053 <xxg3053@qq.com>
2016-09-30 14:25:19 +0800

    add controller base

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # core
+
+## 要求
+
+需要定义 VIEW_BASE_PATH   
+eg: define('VIEW_BASE_PATH', BASE_PATH.'/app/views/');  
---
+++ src/TinyKen/Controllers/BaseController.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kenfo
+ * Date: 2016/9/30
+ * Time: 14:22
+ */
+namespace TinyKen\Controllers;
+
+class BaseController
+{
+    public function __construct()
+    {
+    }
+    public function __destruct()
+    {
+    }
+}
